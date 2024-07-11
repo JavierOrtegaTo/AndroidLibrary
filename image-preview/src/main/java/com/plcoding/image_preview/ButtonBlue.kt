3537bb7233d5aab9b0ff1c963198546a5e6129dd
JavierOrtegaTobar <jeortegatobar@gmail.com>
2024-07-11 17:58:18 -0400

    test

--- image-preview/src/main/java/com/plcoding/image_preview/ButtonBlue.kt
+++ image-preview/src/main/java/com/plcoding/image_preview/ButtonBlue.kt
@@ -4,20 +4,20 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.widget.Button
+import android.widget.LinearLayout
 import android.widget.Toast
-import androidx.constraintlayout.widget.ConstraintLayout
 
-class ButtonBlue @JvmOverloads constructor(
+class Modal @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyle: Int = 0,
     defStyleRes: Int = 0,
-) : ConstraintLayout(context, attrs, defStyle, defStyleRes) {
+) : LinearLayout(context, attrs, defStyle, defStyleRes) {
 
     private val btn: Button
 
     init {
-        LayoutInflater.from(context).inflate(R.layout.component_button_blue, this, true)
+        LayoutInflater.from(context).inflate(R.layout.modal_test_libreria, this, true)
         btn = findViewById(R.id.btn_blue)
         btn.setOnClickListener {
             Toast.makeText(context, "wenas gente", Toast.LENGTH_SHORT).show()
