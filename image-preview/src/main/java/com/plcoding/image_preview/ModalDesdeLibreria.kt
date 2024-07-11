4204591ab4cecee22f48e4d2d0e2adce59fbb7c1
JavierOrtegaTobar <jeortegatobar@gmail.com>
2024-07-11 01:31:41 -0400

    test 4000

--- image-preview/src/main/java/com/plcoding/image_preview/ModalDesdeLibreria.kt
+++ image-preview/src/main/java/com/plcoding/image_preview/ModalDesdeLibreria.kt
@@ -1,12 +1,12 @@
 package com.plcoding.image_preview
 
-import android.app.DialogFragment
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
+import androidx.fragment.app.DialogFragment
 
 class ModalDesdeLibreria : DialogFragment() {
 
