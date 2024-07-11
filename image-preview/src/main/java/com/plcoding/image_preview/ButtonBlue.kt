8d59ec756ec83dc0ae1bed05dec43f0745dcb476
JavierOrtegaTobar <jeortegatobar@gmail.com>
2024-07-11 00:22:48 -0400

    test 4000

--- image-preview/src/main/java/com/plcoding/image_preview/ButtonBlue.kt
+++ image-preview/src/main/java/com/plcoding/image_preview/ButtonBlue.kt
@@ -4,16 +4,15 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.widget.Button
-import android.widget.ImageButton
+import android.widget.LinearLayout
 import android.widget.Toast
-import androidx.constraintlayout.widget.ConstraintLayout
 
 class ButtonBlue @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyle: Int = 0,
     defStyleRes: Int = 0,
-) : ConstraintLayout(context, attrs, defStyle, defStyleRes){
+) : LinearLayout(context, attrs, defStyle, defStyleRes){
 
     init {
         LayoutInflater.from(context).inflate(R.layout.component_button_blue, this, true)
