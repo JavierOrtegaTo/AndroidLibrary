51fd898ef32818acd1bfe025520730d9ef014d01
JavierOrtegaTobar <jeortegatobar@gmail.com>
2024-07-11 17:52:27 -0400

    Revert "test 3000"
    This reverts commit f206b7951e54813b0c697794112607309caf111d.

--- image-preview/src/main/java/com/plcoding/image_preview/ButtonBlue.kt
+++ image-preview/src/main/java/com/plcoding/image_preview/ButtonBlue.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.widget.Button
-import android.widget.ImageButton
 import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
 
@@ -13,13 +12,19 @@ class ButtonBlue @JvmOverloads constructor(
     attrs: AttributeSet? = null,
     defStyle: Int = 0,
     defStyleRes: Int = 0,
-) : ConstraintLayout(context, attrs, defStyle, defStyleRes){
+) : ConstraintLayout(context, attrs, defStyle, defStyleRes) {
+
+    private val btn: Button
 
     init {
         LayoutInflater.from(context).inflate(R.layout.component_button_blue, this, true)
-        val btn = findViewById<Button>(R.id.btn_blue)
+        btn = findViewById(R.id.btn_blue)
         btn.setOnClickListener {
             Toast.makeText(context, "wenas gente", Toast.LENGTH_SHORT).show()
         }
+    }
+
+    override fun setEnabled(enabled: Boolean) {
+        btn.isEnabled = enabled
     }
 }
